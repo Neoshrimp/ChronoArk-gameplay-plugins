608b01ee775c322b12f78f4aadb761656f0f9eca
Neoshrimp <shadow638@protonmail.com>
2021-08-30 01:37:22 +0300

    Add more cursed battles plugin files

---
+++ src/More-cursed-battles/MoreCursedBattlesPlugin.cs
@@ -0,0 +1,150 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using GameDataEditor;
+using HarmonyLib;
+using System.Collections.Generic;
+using TileTypes;
+using UnityEngine;
+
+
+namespace More_cursed_battles
+{
+    [BepInPlugin(GUID, "More cursed battles", version)]
+    [BepInProcess("ChronoArk.exe")]
+    public class MoreCursedBattlesPlugin : BaseUnityPlugin
+    {
+        public const string GUID = "org.neo.chronoark.runmutators.morecursedbattles";
+        public const string version = "0.8.0";
+
+        private static readonly Harmony harmony = new Harmony(GUID);
+
+        private static ConfigEntry<int> startingLiftingAmountConf;
+        private static ConfigEntry<int> cursedBattleNumberConf;
+        private static ConfigEntry<bool> enabledInSanctuary;
+        private static ConfigEntry<int> cursedGoldReward;
+
+
+
+
+        void Awake()
+        {
+            cursedBattleNumberConf = Config.Bind("Generation config", "number_of_cursed_battles_per_stage", 2, "Maximum number of cursed battles per stage counting default one. Set to 5 to curse all non-boss battles on every stage");
+            enabledInSanctuary = Config.Bind("Generation config", "cursed_battles_in_Sanctuary", false, "ATTENTION! currently isn't implemented properly. Keep it off\nEnables/Disables cursed fight generation in the final area.");
+
+            startingLiftingAmountConf = Config.Bind("Item config", "starting_lifting_scroll_amount", 2, "Amount of starting lifting scrolls. Lifting scrolls are identified. Mind that 1-2 cursed fights no longer drop lifting scrolls");
+            cursedGoldReward = Config.Bind("Item config", "cursed_gold_reward", 150, "Sets the amount of gold commonly rewarded by the cursed enemies. Vanilla amount is 250");
+
+            harmony.PatchAll();
+        }
+        void OnDestroy()
+        {
+            if (harmony != null)
+                harmony.UnpatchAll(GUID);
+        }
+
+        //TODO
+        //correctly generate curses for dual buildings in sanctuary
+        //sanctuary dual golem mobs bug out with extra action
+        //patch shared health buff to work with different max hp amounts
+        //add lift curse cards to bottom of the hand (or give an option to)
+
+
+        //maybe reduce particle intensity on cursed enemies and/or map objects
+
+
+
+
+        // add starting lifting scrolls
+        [HarmonyPatch(typeof(FieldSystem))]
+        class FieldSystem_Patch
+        {
+            [HarmonyPatch(nameof(FieldSystem.StageStart))]
+            [HarmonyPrefix]
+            static void StageStartPrefix()
+            {
+                // copied from FieldSystem.StageStart
+                if (PlayData.TSavedata.StageNum == 0 && !PlayData.TSavedata.GameStarted)
+                {
+                    // identifies lifting scroll
+                    if (PlayData.TSavedata.IdentifyItems.Find((string x) => x == GDEItemKeys.Item_Scroll_Scroll_Uncurse) == null)
+                        PlayData.TSavedata.IdentifyItems.Add(GDEItemKeys.Item_Scroll_Scroll_Uncurse);
+
+                    if (startingLiftingAmountConf.Value > 0)
+                        PartyInventory.InvenM.AddNewItem(ItemBase.GetItem(GDEItemKeys.Item_Scroll_Scroll_Uncurse, startingLiftingAmountConf.Value));
+                }
+            }
+
+        }
+
+        // Fisher–Yates shuffle
+        private static void KnuthShuffle<T>(List<T> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                int j = Random.Range(i, list.Count); // Don't select from the entire array on subsequent loops
+                T temp = list[i]; list[i] = list[j]; list[j] = temp;
+            }
+        }
+
+        [HarmonyPatch(typeof(StageSystem))]
+        class Generate_Cursed_Battles_Patch
+        {
+            [HarmonyPatch(nameof(StageSystem.InstantiateIsometric))]
+            [HarmonyPostfix]
+            static void InstantiateIsometric(HexMap ___Map)
+            {
+                if (!PlayData.TSavedata.IsLoaded)
+                {
+                    bool isSanctuary = PlayData.TSavedata.StageNum == 5;
+
+                    if ((isSanctuary && enabledInSanctuary.Value) || !isSanctuary)
+                    {
+                        // checks are required for both tile battles and building battles
+                        List<MapTile> battleList =
+                            ___Map.EventTileList.FindAll(x => (x.Info.Type is Monster) ||
+                            (x.TileEventObject != null && x.TileEventObject.ObjectData != null && x.TileEventObject.Monster));
+                        int curseCount = battleList.FindAll(x => x.Info.Cursed == true).Count;
+
+                        KnuthShuffle(battleList);
+                        foreach (MapTile mt in battleList)
+                        {
+                            if (curseCount >= cursedBattleNumberConf.Value)
+                                break;
+                            if (mt.Info.Cursed == false)
+                            {
+                                mt.Info.Cursed = true;
+                                curseCount++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(B_CursedMob))]
+        class Curse_Reward_Patch
+        {
+            [HarmonyPatch(nameof(B_CursedMob.Init))]
+            [HarmonyPostfix]
+            static void InitPostfix(B_CursedMob __instance, List<ItemBase> ___Itemviews)
+            {
+                void addGold(int amount)
+                {
+                    if (amount > 0)
+                        ___Itemviews.Add(ItemBase.GetItem(GDEItemKeys.Item_Misc_Gold, amount));
+                }
+                // ItemBase doesn't implement Equals so Remove doesn't work lel
+                if (___Itemviews.RemoveAll(x => x.itemkey == GDEItemKeys.Item_Misc_Gold && x.StackCount == 250) > 0)
+                    addGold(cursedGoldReward.Value);
+                
+                if (PlayData.TSavedata.StageNum == 1)
+                {
+                    ___Itemviews.Clear();
+                    addGold(cursedGoldReward.Value);
+                }
+            }
+        }
+
+
+    }
+}

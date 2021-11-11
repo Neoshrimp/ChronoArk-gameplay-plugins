2b0196ee10e4374ff21d2ca0226591096cc6b80c
Neoshrimp <shadow638@protonmail.com>
2021-11-12 01:14:15 +0200

    Change difficulty tooltip text about acc cap

--- src/Remove-hitcap/Remove_hitcap_plugin.cs
+++ src/Remove-hitcap/Remove_hitcap_plugin.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 using BepInEx.Configuration;
+using I2.Loc;
+using System.Reflection;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.RegularExpressions;
 
 namespace Remove_hitcap
 {
@@ -36,9 +42,55 @@ namespace Remove_hitcap
         }
 
 
-    //TODO
-    //tweak expert tooltip
+        //TODO
+        //language change requires restart maybe? but changing languages mid game is no perfect in vanilla
+        //expert icon in party select screen still has old description remaining
 
+
+
+        static string ProcessTooltip(string tooltip)
+        {
+            return new Regex("(?!\n).*?98.*?\n").Replace(tooltip, "");
+        }
+
+
+        [HarmonyPatch]
+        class DifficultySelectTooltipPatch
+        {
+            static MethodBase TargetMethod()
+            {
+                return AccessTools.PropertyGetter(typeof(ScriptLocalization.UI_SelectParty), "OriginDesc");
+            }
+
+            static void Postfix(ref string __result)
+            {
+                __result = ProcessTooltip(__result);
+            }
+        }
+
+        [HarmonyPatch(typeof(PartyInventory), "Awake")]
+        class ExpertIconPatch
+        {
+            static void Postfix(PartyInventory __instance)
+            {
+
+                if (__instance.ExpertIcon != null)
+                {
+
+                    SimpleTooltip tooltip = __instance.ExpertIcon.GetComponent<SimpleTooltip>();
+                    if (tooltip != null)
+                    {
+                        tooltip.TooltipString = ProcessTooltip(tooltip.ToolTipString_l2);
+                        tooltip.ToolTipString_l2 = null;
+                    }
+
+                }
+            }
+        }
+
+
+
+        // actual logic
         [HarmonyPatch(typeof(BattleChar), nameof(BattleChar.HitPerNum))]
         class HitcapPatch
         {
@@ -67,6 +119,7 @@ namespace Remove_hitcap
                 //Debug.Log(changeCount);
             }
         }
+
 
 
     }

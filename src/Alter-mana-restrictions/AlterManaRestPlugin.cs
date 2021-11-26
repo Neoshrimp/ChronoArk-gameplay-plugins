249f8b7fd1ee5452c583e7a9423585478d4de093
Neoshrimp <shadow638@protonmail.com>
2021-11-26 20:30:08 +0200

    Add better tooltips for alter mana restrictions

--- src/Alter-mana-restrictions/AlterManaRestPlugin.cs
+++ src/Alter-mana-restrictions/AlterManaRestPlugin.cs
@@ -1,9 +1,11 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
+using I2.Loc;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Reflection.Emit;
 using UnityEngine;
 
@@ -29,10 +31,14 @@ namespace Alter_mana_restrictions
         private static ConfigEntry<int> firstManaRestriction;
         private static ConfigEntry<int> secondManaRestriction;
 
+        private static string tooltip1 = ScriptLocalization.UI.MPUpLv_1;
+        private static string tooltip2 = ScriptLocalization.UI.MPUpLv_2;
+
+
 
         void Awake()
         {
- 
+
             firstManaRestriction = configFile.Bind("Restriction config", "first restriction mana value", -1, "Mana value where 3 characters at level 2+ restriction will be applied. Example, setting to 3 won't allow any mana level ups until each of the 3 characters reach level 2. Set below 3 to disable");
             secondManaRestriction = configFile.Bind("Restriction config", "second restriction mana value", -1, "Mana value where 4 characters at level 3+ restriction will be applied. Set below 3 to disable. Should be higher than first restriction unless disabled");
 
@@ -44,6 +50,9 @@ namespace Alter_mana_restrictions
                 secondRest = firstRest + 1;
             }
             logger = Logger;
+
+            setTooltips();
+
             harmony.PatchAll();
         }
         void OnDestroy()
@@ -51,10 +60,34 @@ namespace Alter_mana_restrictions
             if (harmony != null)
                 harmony.UnpatchAll(GUID);
         }
+\
 
-        //TODO
-        //maybe display requirement tooltip even if soulstone condition isn't met
-        //maybe fix tooltip logic
+        static void setTooltips()
+        {
+            if (LocalizationManager.CurrentLanguage == "English")
+            {
+                tooltip1 = "You must have at least 3 party members at or above level 2.";
+                tooltip2 = "You must have 4 party members at or above level 3.";
+            }
+            else
+            {
+                tooltip1 = ScriptLocalization.UI.MPUpLv_1;
+                tooltip2 = ScriptLocalization.UI.MPUpLv_2;
+            }
+        }
+
+        [HarmonyPatch]
+        class ManaTooptipPatch
+        {
+            static MethodBase TargetMethod()
+            {
+                return AccessTools.PropertySetter(typeof(LocalizationManager), "CurrentLanguage");
+            }
+            static void Postfix()
+            {
+                setTooltips();
+            }
+        }
 
 
         [HarmonyPatch(typeof(CharStatV3), "Update")]
@@ -87,7 +120,22 @@ namespace Alter_mana_restrictions
                     prevInst = i;
 
                 }
+            }
 
+            static void Postfix(CharStatV3 __instance)
+            {
+                if (PlayData.TSavedata.SoulUpgrade.AP == firstRest && PlayData.TSavedata.Party.FindAll((Character a) => a.LV >= 2).Count < 3)
+                {
+                    __instance.MPTooltip.enabled = true;
+                    __instance.MPTooltip.TooltipString = tooltip1;
+
+
+                }
+                else if (PlayData.TSavedata.SoulUpgrade.AP == 2 && PlayData.TSavedata.Party.Find((Character a) => a.LV <= 2) != null)
+                {
+                    __instance.MPTooltip.enabled = true;
+                    __instance.MPTooltip.TooltipString = tooltip2;
+                }
             }
         }
 

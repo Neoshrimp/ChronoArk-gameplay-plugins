0b6e3757cdfb6685a193301e3a07604bb1166849
Neoshrimp <shadow638@protonmail.com>
2022-06-02 23:30:17 +0300

    Almost add limited curse lifting mode

--- src/More-cursed-battles/MoreCursedBattlesPlugin.cs
+++ src/More-cursed-battles/MoreCursedBattlesPlugin.cs
@@ -10,6 +10,7 @@ using System.Reflection;
 using System.Reflection.Emit;
 using TileTypes;
 using UnityEngine;
+using BepInEx.Logging;
 using Random = UnityEngine.Random;
 
 
@@ -24,10 +25,14 @@ namespace More_cursed_battles
 
         private static readonly Harmony harmony = new Harmony(GUID);
 
+        public static BepInEx.Logging.ManualLogSource logger;
+
         private static ConfigEntry<int> startingLiftingAmountConf;
         private static ConfigEntry<int> cursedBattleNumberConf;
         private static ConfigEntry<bool> enabledInSanctuary;
         private static ConfigEntry<bool> enabledInCW;
+        private static ConfigEntry<bool> limitedUncurse;
+
 
         private static ConfigEntry<int> cursedGoldReward;
         private static ConfigEntry<bool> betterCursedRewardsInSanctuary;
@@ -46,13 +51,23 @@ namespace More_cursed_battles
             enabledInSanctuary = Config.Bind("Generation config", "cursed_battles_in_Sanctuary", true, "Enables/Disables cursed fight generation in the final area. (acceptable values: true/false)");
             enabledInCW = Config.Bind("Generation config", "cursed_battles_in_Crimson_Wilderness", true, "Enables/Disables cursed fight generation in Crimson Wilderness. (acceptable values: true/false)");
             cwMiniBossCurseChance = Config.Bind("Generation config", "exclusive_curse_miniBosses_in_Crimson_Wilderness", true, "In Crimson Wilderness cursed fights the cursed mob will always be a miniboss. Else cursed mob is random (acceptable values: true/false)");
-
+            limitedUncurse = Config.Bind("Generation config", "limited_uncurse_mode_enabled", false, "Items obtained at the beginning of the run are special. Only they can be used to remove curse mid-battle. Regular lifting scrolls can only be used to uncurse equipment and find secret tiles! Use starting_lifting_scroll_amount to set the amount of special items obtained. (acceptable values: true/false)");
 
             startingLiftingAmountConf = Config.Bind("Item config", "starting_lifting_scroll_amount", 2, "Amount of starting lifting scrolls. Lifting scrolls are identified. Mind that 1-2 cursed fights no longer drop lifting scrolls.");
             cursedGoldReward = Config.Bind("Item config", "cursed_gold_reward", 100, "Sets the amount of gold commonly rewarded by killing cursed enemies. Vanilla amount is 200.");
             betterCursedRewardsInSanctuary = Config.Bind("Item config", "enable_better_cursed_rewards_in_Sanctuary", true, "Cursed enemies in Sanctuary drop better rewards like potions or rare items. (acceptable values: true/false)");
             betterCursedRewardsInCW = Config.Bind("Item config", "enable_better_cursed_rewards_in_Crimson_Wilderness", true, "Cursed enemies in Crimson Wilderness drop better rewards. (acceptable values: true/false)");
             restoreUncommonRewards = Config.Bind("Item config", "restore_uncommon_rewards", true, "Make certain enemies drop blue equipment as they used to (acceptable values: true/false)");
+
+            logger = Logger;
+
+            var meth = AccessTools.Method(typeof(System.IO.Directory), nameof(System.IO.Directory.GetFiles), new Type[] { typeof(string), typeof(string), typeof(System.IO.SearchOption) });
+            var meth2 = AccessTools.Method(typeof(System.Linq.Enumerable), nameof(System.Linq.Enumerable.ToList));
+
+            Debug.Log(meth);
+            Debug.Log(meth2);
+
+
 
             harmony.PatchAll();
         }
@@ -63,7 +78,7 @@ namespace More_cursed_battles
         }
 
         //TODO
-        //add limited lifting mode
+        //add custom item icon
 
         //loading removes original red particles
         //cursed lightning hedgehogs produces exception
@@ -85,12 +100,20 @@ namespace More_cursed_battles
                 // copied from FieldSystem.StageStart
                 if (PlayData.TSavedata.StageNum == 0 && !PlayData.TSavedata.GameStarted)
                 {
-                    // identifies lifting scroll
-                    if (PlayData.TSavedata.IdentifyItems.Find((string x) => x == GDEItemKeys.Item_Scroll_Scroll_Uncurse) == null)
-                        PlayData.TSavedata.IdentifyItems.Add(GDEItemKeys.Item_Scroll_Scroll_Uncurse);
+                    if (!limitedUncurse.Value)
+                    {
+                        // identifies lifting scroll
+                        if (PlayData.TSavedata.IdentifyItems.Find((string x) => x == GDEItemKeys.Item_Scroll_Scroll_Uncurse) == null)
+                            PlayData.TSavedata.IdentifyItems.Add(GDEItemKeys.Item_Scroll_Scroll_Uncurse);
+                        if (startingLiftingAmountConf.Value > 0)
+                            PartyInventory.InvenM.AddNewItem(ItemBase.GetItem(GDEItemKeys.Item_Scroll_Scroll_Uncurse, startingLiftingAmountConf.Value));
+                    }
+                    else
+                    {
+                        if (startingLiftingAmountConf.Value > 0)
+                            PartyInventory.InvenM.AddNewItem(ItemBase.GetItem(MiscSpecialUncurseKey, startingLiftingAmountConf.Value));
 
-                    if (startingLiftingAmountConf.Value > 0)
-                        PartyInventory.InvenM.AddNewItem(ItemBase.GetItem(GDEItemKeys.Item_Scroll_Scroll_Uncurse, startingLiftingAmountConf.Value));
+                    }
                 }
             }
 
@@ -121,9 +144,6 @@ namespace More_cursed_battles
                 {
                     bool isSanctuary = ___Map.StageData.Key == GDEItemKeys.Stage_Stage4;
                     bool isCrimson = ___Map.StageData.Key == GDEItemKeys.Stage_Stage_Crimson;
-
-                    Debug.Log(isSanctuary);
-                    Debug.Log(isCrimson);
 
                     if ((!isSanctuary && !isCrimson) || (isSanctuary && enabledInSanctuary.Value) || (isCrimson && enabledInCW.Value))
                     {
@@ -216,7 +236,7 @@ namespace More_cursed_battles
 
 
         [HarmonyPatch(typeof(B_CursedMob))]
-        class Curse_Reward_Patch
+        class Cursed_Reward_Patch
         {
             static int vanillaGoldReward;
 
@@ -381,6 +401,135 @@ namespace More_cursed_battles
                         yield return ci;
                     }
                     i++;
+                }
+            }
+        }
+
+        public static readonly string ScrollSpecialUncurseKey = "ScrollSpecialUncurse";
+        public static readonly string MiscSpecialUncurseKey = "MiscSpecialUncurse";
+
+
+        [HarmonyPatch(typeof(GDEDataManager), nameof(GDEDataManager.BuildDataKeysBySchemaList))]
+        class RegisterCustomKeysPatch
+        {
+
+            static void UpdateGDEDataStructures(string schema, string key, AccessTools.FieldRef<GDEDataManager, Dictionary<string, HashSet<string>>> dataKeysBySchemaRef)
+            {
+
+                dataKeysBySchemaRef()[schema].Add(key);
+
+                try
+                {
+                    var schemaType = Type.GetType("GameDataEditor." + "GDE" + schema + "Data, " + typeof(FieldSystem).Assembly);
+                    var schemaCtor = AccessTools.Constructor(schemaType, new Type[] { typeof(string) });
+                    var data = schemaCtor.Invoke(new string[] { key });
+                    var saveToDictMethod = AccessTools.Method(schemaType, "SaveToDict", new Type[] { });
+                    var dict = (Dictionary<string, object>)saveToDictMethod.Invoke(data, new object[] { });
+
+                    GDEDataManager.masterData.TryAddOrUpdateValue(key, dict);
+                }
+                catch (Exception e)
+                {
+                    logger.LogError($"Error to add (schema: {schema}, key: {key}) to masterdata");
+                    logger.LogError(e);
+                }
+            }
+            static void Postfix()
+            {
+                var dataKeysBySchemaRef = AccessTools.FieldRefAccess<GDEDataManager, Dictionary<string, HashSet<string>>>(AccessTools.Field(typeof(GDEDataManager), "dataKeysBySchema"));
+
+                if (dataKeysBySchemaRef() == null)
+                {
+                    logger.LogError("Ref is null");
+                    return;
+                }
+                //UpdateGDEDataStructures(GDESchemaKeys.Item_Scroll, ScrollSpecialUncurseKey, dataKeysBySchemaRef);
+                UpdateGDEDataStructures(GDESchemaKeys.Item_Misc, MiscSpecialUncurseKey, dataKeysBySchemaRef);
+
+
+            }
+        }
+
+        [HarmonyPatch(typeof(GDEItem_MiscData), nameof(GDEItem_MiscData.LoadFromSavedData))]
+        class ConsumableDataPatch
+        {
+            static void Postfix(GDEItem_MiscData __instance)
+            {
+                if (__instance.Key == MiscSpecialUncurseKey)
+                {
+                    __instance.name = "Greater Lifting";
+                    __instance.stack = true;
+                    __instance.outinventory = false;
+                    __instance.image = new Texture2D(1, 1);
+                    __instance.Description = "Create Lift Curse card at the start of a cursed battle";
+                    __instance.MaxStack = 99;
+                    __instance.Price = 500;
+                    __instance.Itemclass = new GDEItemClassData(GDEItemKeys.ItemClass_Legendary);
+
+                }
+            }
+        }
+
+        [HarmonyPatch]
+        class EnableLimitedUncurse0
+        {
+            static string CheckLimitedUncurseConfig()
+            {
+                if (limitedUncurse.Value)
+                    return MiscSpecialUncurseKey;
+                return GDEItemKeys.Item_Scroll_Scroll_Uncurse;
+            }
+
+            static IEnumerable<MethodBase> TargetMethods()
+            {
+                yield return AccessTools.Method(typeof(B_CursedMob), nameof(B_CursedMob.BattleStart));
+                yield return AccessTools.Method(typeof(SkillExtended_UnCurse), nameof(SkillExtended_UnCurse.SkillUseSingle));
+            }
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                foreach (var ci in instructions)
+                {
+                    if (ci.Is(OpCodes.Ldsfld, AccessTools.Field(typeof(GDEItemKeys), nameof(GDEItemKeys.Item_Scroll_Scroll_Uncurse))))
+                    {
+                        yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(EnableLimitedUncurse0), nameof(EnableLimitedUncurse0.CheckLimitedUncurseConfig)));
+                    }
+                    else
+                    {
+                        yield return ci;
+                    }
+                }
+            }
+            
+        }
+
+        [HarmonyPatch]
+        class EnableLimitedUncurse1
+        {
+
+            static string CheckLimitedUncurseConfig(string arg0)
+            {
+                if (limitedUncurse.Value)
+                    return arg0;
+                return GDEItemKeys.Item_Scroll_Scroll_Uncurse;
+            }
+            static MethodInfo TargetMethod()
+            {
+                return AccessTools.Method(typeof(B_CursedMob), "<BattleStart>m__0");
+            }
+
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                foreach (var ci in instructions)
+                {
+                    if (ci.Is(OpCodes.Ldsfld, AccessTools.Field(typeof(GDEItemKeys), nameof(GDEItemKeys.Item_Scroll_Scroll_Uncurse))))
+                    {
+                        yield return new CodeInstruction(OpCodes.Ldarg_0);
+                        yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(EnableLimitedUncurse1), nameof(EnableLimitedUncurse1.CheckLimitedUncurseConfig)));
+                    }
+                    else
+                    {
+                        yield return ci;
+                    }
                 }
             }
 
